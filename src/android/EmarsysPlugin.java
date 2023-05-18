ba4751f791d76d6fa100b9ab0e86cfc8d7272c9d
anikethhippo <aniketh.keshava@hippoinnovations.com>
2023-05-18 17:17:08 +0530

    Emarsys revert

--- src/android/EmarsysPlugin.java
+++ src/android/EmarsysPlugin.java
@@ -35,7 +35,7 @@ public class EmarsysPlugin extends CordovaPlugin {
         //String value  = arg_object.getString("value");
         //Map<String, String> map = new HashMap<String, String>();
        // map.put(key,value);
-        Emarsys.setContact(contactValue);     
+        Emarsys.setContact(contactFieldID,contactValue);     
         //EmarsysApplication.onCreate(applicationCode,merchantID)
         return true;
 
